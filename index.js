c9e252f907af5e623f73a90c41dd7b992e7363f6
Abhi Agarwal <me@abhiagarwal.com>
2015-04-26 14:49:12 -0400

    Splitting up shaGenerate method

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ var request = require('request-promise');
 var sys = require('sys')
 var exec = require('child_process').exec;
 
+var puts = function(error, stdout, stderr) { 
+  sys.puts(stdout) 
+};
+
+var shaGenerate = function(user) {
+  return sha1(sha1(sha1(user.id), sha1(user.facebookId)), sha1(user.created))
+};
+
+var generatePassword = function(user) {
+  if (user && user.roles && user.roles.length != 0) {
+    var userName = user.name.replace(/\s/g, '');
+    var userPass = shaGenerate(user);
+    var commandToDo = "--user=" + userName + " --password=" + userPass;
+    exec("php /var/www/html/maintenance/changePassword.php " + commandToDo, puts);
+  }
+};
+
 request({
   url: 'https://api.tnyu.org/v2/people',
   rejectUnauthorized: false,
@@ -13,15 +30,3 @@ request({
   var people = JSON.parse(peopleBody).data;
   people.forEach(generatePassword);
 });
-
-var generatePassword = function(user) {
-  if (user && user.roles && user.roles.length != 0) {
-    var userName = user.name.replace(/\s/g, '');
-    var userPass = sha1(sha1(sha1(user.id), sha1(user.facebookId)), sha1(user.created));
-    console.log(userName, userPass);
-    var commandToDo = "php /var/www/html/maintenance/changePassword.php --user=" + userName + " --password=" + userPass
-    exec(commandToDo, puts);
-  }
-};
-
-function puts(error, stdout, stderr) { sys.puts(stdout) }
